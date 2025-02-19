0a745aeb5c90834edecc2a3740423c4fb079aef7
Chris R <136107779+CWOChris@users.noreply.github.com>
2025-02-18 21:24:40 -0600

    got the sign up and login to work correctly"

---
+++ apertum-ostium/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+    return (
+        <div>
+            <h1>Home</h1>
+            <p>Welcome to Apertum Ostium</p>
+        </div>
+    )
+};
+
+export default Home;
---
+++ apertum-ostium/src/pages/Log.jsx
@@ -0,0 +1,10 @@
+const Log = () => {
+    return (
+        <div>
+            <h1>Log</h1>
+            <p>This is the Log page.</p>
+        </div>
+    )
+};
+
+export default Log;
